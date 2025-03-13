628f1ff47b5313130e21d960eb6c13bda2d74eac
Dhruv Gupta <76417670+dhruvgup1a@users.noreply.github.com>
2025-03-13 04:52:25 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from src.rag_pipeline import *
+
+def main():
+    
+    theme = "Social Isolation"
+    run_rag_pipeline(theme)
+
+    print("\n=======================\n= EVERYTHING WORKS!!! =\n=======================\n")
+
+
+if __name__ == "__main__":
+    main()
